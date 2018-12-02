f71f4210f26a0502b1d5ef99ede926de50ea4686
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2018-12-02 15:36:04 +0900

    [UPDATE] add find destination operation

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -3,10 +3,10 @@ import java.sql.*;
 public class PathFinder {
 	private String startPoint;
 	private String destination;
-	private Food[] recommanded;
+	private Food recommanded;
 	private RoadMap map;
 	
-	public PathFinder(String start, Food[] f) {
+	public PathFinder(String start, Food f) {
 		startPoint = start;
 		recommanded = f;
 		map = new RoadMap();
@@ -26,7 +26,26 @@ public class PathFinder {
 		}
 		
 		// main operation
-		// TODO: making finding destination operation by food
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = null;
+			String sql = "select store_location from store where store_location_no = "
+					+ "'" + recommanded.getStore_id() + "'";
+			rs = stmt.executeQuery(sql);
+			
+			if (rs.next())
+				destination = rs.getString(1);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		// close database
+		try {
+			if (con != null && !con.isClosed())
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		// close database
 		try {
