1f54a3642c62ff49851f11ca251d7fbafb306ce5
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2018-12-02 14:08:42 +0900

    [ADD] add dijkstra part source code

---
+++ src/PathFinder.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.sql.*;
+
+public class PathFinder {
+	private String startPoint;
+	private String destination;
+	private Food[] recommanded;
+	private RoadMap map;
+	
+	public PathFinder(String start, Food[] f) {
+		startPoint = start;
+		recommanded = f;
+		map = new RoadMap();
+		
+		// open database
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/timeLocker_DB";
+			String user = "root", password = "zmfhzjm0";
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println(con);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// main operation
+		// TODO: making finding destination operation by food
+		
+		// close database
+		try {
+			if (con != null && !con.isClosed())
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String[] findShortestWay() {
+		map.setStartAndEnd(startPoint, destination);
+		String[] result = map.dijkstra();
+		
+		return result;
+	}
+}
---
+++ src/RoadMap.java
@@ -0,0 +1,141 @@
+package main;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Vector;
+
+public class RoadMap {
+	public static final int nodeNum = 19;
+	private String[] nodes; // 각 노드의 이름
+	private int[][] links; // 연결된 path
+	private int[] distances; // 각 번호 노드의 최단거리
+	private int[] prevNode; // 전임자 노드
+	private boolean[] checks; // 방문 여부 체크 변수
+	private int startingPoint; // 시작점
+	private int destination; // 도착점
+	// 경로 데이터 저장 변수
+	static Vector<Integer> steps = new Vector<Integer>();
+	enum Nodes { // 지도 정보를 위한 enumeration
+		A("기숙사"), B("학생회관"), C("중도"), D("교대"), E("바나대"),
+		F("대학원"), G("예대1"), H("공대1"), I("글센"), J("IT대"),
+		K("가천관"), L("산학"), M("공대2"), N("바나연"), O("법대"),
+		P("비타"), Q("스타덤"), R("식당가"), S("학군단");
+		
+		private final String place;
+		
+		private Nodes(String placeName) {
+			place = placeName;
+		}
+		
+		public String getPlace() {
+			return place;
+		}
+	}
+	
+	public RoadMap() {
+		// initialization
+		nodes = new String[nodeNum];
+		for (int i = 0; i < nodeNum; i++)
+			nodes[i] = Nodes.values()[i].getPlace();
+		links = new int[nodeNum][nodeNum];
+		for (int i = 0; i < nodeNum; i++)
+			Arrays.fill(links[i], Integer.MAX_VALUE);
+		distances = new int[nodeNum];
+		Arrays.fill(distances, Integer.MAX_VALUE);
+		checks = new boolean[nodeNum];
+		Arrays.fill(checks, false);
+		prevNode = new int[nodeNum];
+		Arrays.fill(prevNode, 0);
+		
+		String fileName = "datalist.txt";
+		Scanner fis = null;
+		try {
+			fis = new Scanner(new File(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		int value;
+		String start, end;
+		while (fis.hasNextLine()) {
+			value = fis.nextInt();
+			start = fis.next();
+			end = fis.next();
+			int startI = 0, endI = 0;
+			
+			boolean sflag = false, eflag = false;
+			for (int i = 0; i < nodeNum; i++) {
+				if (sflag && eflag)
+					break;
+				if (nodes[i].equalsIgnoreCase(start)) {
+					startI = i;
+					sflag = true;
+				}
+				if (nodes[i].equalsIgnoreCase(end)) {
+					endI = i;
+					eflag = true;
+				}
+			}
+			
+			links[startI][endI] = value;
+			links[endI][startI] = value;
+		}
+	}
+	
+	public void setStartAndEnd(String start, String end) {
+		for (int i = 0; i < nodeNum; i++) {
+			if (start.equalsIgnoreCase(nodes[i])) {
+				startingPoint = i;
+			}
+			if (end.equalsIgnoreCase(nodes[i])) {
+				destination = i;
+			}
+		}
+	}
+	
+	private int shortestPath(int start, int end) {
+		if (start == end)
+			return distances[end];
+		
+		checks[start] = true;
+		
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < nodeNum; i++) {
+			if (distances[i] > distances[start] + links[start][i]) {
+				distances[i] = distances[start] + links[start][i];
+				prevNode[i] = start;
+			}
+			
+			if (min == Integer.MAX_VALUE || distances[min] > distances[i])
+				min = i;
+		}
+		
+		shortestPath(min, end);
+		
+		return 0;
+	}
+	
+	private void makingPath(int start, int end, int node) {
+		if (node == start) {
+			steps.add(node);
+			return;
+		}
+		
+		makingPath(start, end, prevNode[node]);
+		steps.add(node);
+	}
+	
+	public String[] dijkstra() {
+		String[] pathNodes;
+		
+		shortestPath(startingPoint, destination);
+		makingPath(startingPoint, destination, destination);
+		
+		pathNodes = new String[steps.size()];
+		for (int i = 0; i < steps.size(); i++)
+			pathNodes[i] = nodes[steps.elementAt(i)];
+		
+		return pathNodes;
+	}
+}
